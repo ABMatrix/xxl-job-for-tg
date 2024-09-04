f35c3d101a9227689032e366072d9b4c384328ab
wangjupenng <w875050772@sina.com>
2024-09-04 11:22:19 +0800

    注册逻辑监听

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobApiController.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobApiController.java
@@ -1,5 +1,16 @@
 package com.xxl.job.admin.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.xxl.job.admin.controller.annotation.PermissionLimit;
 import com.xxl.job.admin.core.conf.XxlJobAdminConfig;
 import com.xxl.job.core.biz.AdminBiz;
@@ -8,15 +19,8 @@ import com.xxl.job.core.biz.model.RegistryParam;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.util.GsonTool;
 import com.xxl.job.core.util.XxlJobRemotingUtil;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import cn.hutool.core.util.StrUtil;
 
 /**
  * Created by xuxueli on 17/5/10.
@@ -25,48 +29,55 @@ import java.util.List;
 @RequestMapping("/api")
 public class JobApiController {
 
-    @Resource
-    private AdminBiz adminBiz;
+	@Resource
+	private AdminBiz adminBiz;
 
-    /**
-     * api
-     *
-     * @param uri
-     * @param data
-     * @return
-     */
-    @RequestMapping("/{uri}")
-    @ResponseBody
-    @PermissionLimit(limit=false)
-    public ReturnT<String> api(HttpServletRequest request, @PathVariable("uri") String uri, @RequestBody(required = false) String data) {
+	/**
+	 * api
+	 *
+	 * @param uri
+	 * @param data
+	 * @return
+	 */
+	@RequestMapping("/{uri}")
+	@ResponseBody
+	@PermissionLimit(limit = false)
+	public ReturnT<String> api(HttpServletRequest request, @PathVariable("uri") String uri,
+			@RequestBody(required = false) String data) {
 
-        // valid
-        if (!"POST".equalsIgnoreCase(request.getMethod())) {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, HttpMethod not support.");
-        }
-        if (uri==null || uri.trim().length()==0) {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, uri-mapping empty.");
-        }
-        if (XxlJobAdminConfig.getAdminConfig().getAccessToken()!=null
-                && XxlJobAdminConfig.getAdminConfig().getAccessToken().trim().length()>0
-                && !XxlJobAdminConfig.getAdminConfig().getAccessToken().equals(request.getHeader(XxlJobRemotingUtil.XXL_JOB_ACCESS_TOKEN))) {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "The access token is wrong.");
-        }
+		// valid
+		if (!"POST".equalsIgnoreCase(request.getMethod())) {
+			return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, HttpMethod not support.");
+		}
+		if (uri == null || uri.trim().length() == 0) {
+			return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, uri-mapping empty.");
+		}
+		if (XxlJobAdminConfig.getAdminConfig().getAccessToken() != null
+				&& XxlJobAdminConfig.getAdminConfig().getAccessToken().trim().length() > 0
+				&& !XxlJobAdminConfig.getAdminConfig().getAccessToken()
+						.equals(request.getHeader(XxlJobRemotingUtil.XXL_JOB_ACCESS_TOKEN))) {
+			String localToken = XxlJobAdminConfig.getAdminConfig().getAccessToken();
+			String tokenParam = request.getHeader(XxlJobRemotingUtil.XXL_JOB_ACCESS_TOKEN);
+			String message = StrUtil.format("The access token is wrong.The local token is{},But the remote token is{}",
+					localToken, tokenParam);
+			return new ReturnT<String>(ReturnT.FAIL_CODE, message);
+		}
 
-        // services mapping
-        if ("callback".equals(uri)) {
-            List<HandleCallbackParam> callbackParamList = GsonTool.fromJson(data, List.class, HandleCallbackParam.class);
-            return adminBiz.callback(callbackParamList);
-        } else if ("registry".equals(uri)) {
-            RegistryParam registryParam = GsonTool.fromJson(data, RegistryParam.class);
-            return adminBiz.registry(registryParam);
-        } else if ("registryRemove".equals(uri)) {
-            RegistryParam registryParam = GsonTool.fromJson(data, RegistryParam.class);
-            return adminBiz.registryRemove(registryParam);
-        } else {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, uri-mapping("+ uri +") not found.");
-        }
+		// services mapping
+		if ("callback".equals(uri)) {
+			List<HandleCallbackParam> callbackParamList = GsonTool.fromJson(data, List.class,
+					HandleCallbackParam.class);
+			return adminBiz.callback(callbackParamList);
+		} else if ("registry".equals(uri)) {
+			RegistryParam registryParam = GsonTool.fromJson(data, RegistryParam.class);
+			return adminBiz.registry(registryParam);
+		} else if ("registryRemove".equals(uri)) {
+			RegistryParam registryParam = GsonTool.fromJson(data, RegistryParam.class);
+			return adminBiz.registryRemove(registryParam);
+		} else {
+			return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, uri-mapping(" + uri + ") not found.");
+		}
 
-    }
+	}
 
 }
